412c9c75698da9d0c2482d0ffaa0ecbd1f40107e
SINGAM BHARGAVI <singambhargvi26@gmail.com>
2023-07-12 12:20:58 +0530

    first commit

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Azure Architect", "Cloud Enginner"],
+        strings: ["DataStructures Expert", "Programmer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
